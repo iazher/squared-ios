883801cabc933882aba2bc20fa79ef2470d9d686
iazher <imanazher00@gmail.com>
2026-09-05 15:32:50 +0100

    Initial commit

---
+++ Squared/SquaredApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquaredApp.swift
+//  Squared
+//
+//  Created by Iman Azher on 05/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SquaredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SquaredTests/SquaredTests.swift
@@ -0,0 +1,19 @@
+//
+//  SquaredTests.swift
+//  SquaredTests
+//
+//  Created by Iman Azher on 05/09/2026.
+//
+
+import Testing
+@testable import Squared
+
+struct SquaredTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
